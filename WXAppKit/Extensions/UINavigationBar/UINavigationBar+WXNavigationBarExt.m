c831599199ec7417279059ba17c17afe5e919abc
sulirong <15528290768@163.com>
2018-01-10 12:18:16 +0800

    if iOS11 is available, modify _UINavigationBarContentView.layoutMargins

--- WXAppKit/Extensions/UINavigationBar/UINavigationBar+WXNavigationBarExt.m
+++ WXAppKit/Extensions/UINavigationBar/UINavigationBar+WXNavigationBarExt.m
@@ -7,8 +7,33 @@
 //
 
 #import "UINavigationBar+WXNavigationBarExt.h"
+#import "WXRuntimeHelper.h"
 
 @implementation UINavigationBar (WXNavigationBarExt)
+
++ (void)load {
+    if (@available(iOS 11.0, *)) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            swizzleInstanceMethod(self, @selector(layoutSubviews), @selector(wx_layoutSubviews));
+        });
+    }
+}
+
+- (void)wx_layoutSubviews {
+    [self wx_layoutSubviews];
+    
+    for (UIView *view in self.subviews) {
+        for (UIView *stackView in view.subviews) {
+            if (@available(iOS 9.0, *)) {
+                if ([stackView isKindOfClass: [UIStackView class]]) {
+                    stackView.superview.layoutMargins = UIEdgeInsetsMake(0, 8, 0, 8);
+                    break;
+                }
+            }
+        }
+    }
+}
 
 - (void)wx_setBarColor:(UIColor *)color {
     
